fd0d13d35ef94a07800e2775003265b9df85479e
rand <p.oscar.franzen@gmail.com>
2019-04-29 13:44:08 +0200

    formating

--- linear_to_matrix.R
+++ linear_to_matrix.R
@@ -1,11 +1,11 @@
 args <- commandArgs(trailingOnly = TRUE)
 
-if(!require(data.table)){
+if (!require(data.table)) {
   install.packages("data.table")
   library(data.table)
 }
 
-if(!require(Matrix)){
+if (!require(Matrix)) {
   install.packages("Matrix")
   library(Matrix)
 }
@@ -15,10 +15,10 @@ out<-args[2]
 
 f<-as.data.frame(fread(file,h=T,sep='\t'))
 
-sm = sparseMatrix(as.numeric(factor(f$gene)), as.numeric(factor(f$cell)), x = f$count)
+sm = sparseMatrix(as.numeric(factor(f$gene)), as.numeric(factor(f$cell)), x=f$count)
 
-rownames(sm) = levels(factor(f$gene))
-colnames(sm) = levels(factor(f$cell))
+rownames(sm)<-levels(factor(f$gene))
+colnames(sm)<-levels(factor(f$cell))
 
 write.table(as.matrix(sm),file=paste0(out,'.txt'),sep='\t',col.names=T,row.names=T,quote=F)
 
