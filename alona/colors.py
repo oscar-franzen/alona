c43208e7ae0f0682eccd45c4f0c0c5d5efeb6a31
rand <p.oscar.franzen@gmail.com>
2019-05-13 10:06:02 +0200

    colors.py

---
+++ alona/colors.py
@@ -0,0 +1,12 @@
+# https://github.com/s0md3v/Photon/blob/master/core/colors.py
+white = '\033[97m'
+green = '\033[92m'
+red = '\033[91m'
+yellow = '\033[93m'
+end = '\033[0m'
+back = '\033[7;91m'
+info = '\033[93m[!]\033[0m'
+que = '\033[94m[?]\033[0m'
+bad = '\033[91m[-]\033[0m'
+good = '\033[92m[+]\033[0m'
+run = '\033[97m[~]\033[0m'
--- alona/logo.py
+++ alona/logo.py
@@ -1,14 +1,16 @@
 import logging
+from .colors import (red,
+                     end)
 
 def show_logo(nologo):
     if not nologo:
         print('''
-  ##   #       ####  #    #   ##   
- #  #  #      #    # ##   #  #  #  
-#    # #      #    # # #  # #    # 
-###### #      #    # #  # # ###### 
-#    # #      #    # #   ## #    # 
-#    # ######  ####  #    # #    # 
-''')
+  %s##%s   #       ####  #    #   ##   
+ %s#  #%s  #      #    # ##   #  #  #  
+%s#    #%s #      #    # # #  # #    # 
+%s######%s #      #    # #  # # ###### 
+%s#    #%s #      #    # #   ## #    # 
+%s#    #%s ######  ####  #    # #    # 
+''' % (red,end,red,end,red,end,red,end,red,end,red,end))
     else:
         logging.debug('hiding logo')
