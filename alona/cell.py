84cf61ede2f08560fe005e3f20ed48819074c403
rand <p.oscar.franzen@gmail.com>
2019-05-15 16:15:19 +0200

    improvements to _remove_mito()

--- alona/cell.py
+++ alona/cell.py
@@ -32,15 +32,15 @@ class Cell():
     def _validate_counts(self):
         """ Basic data validation of gene expression values. """
         if (np.any(self.data.dtypes != 'int64') and
-           self.alonabase.params['dataformat'] == 'raw'):
+                self.alonabase.params['dataformat'] == 'raw'):
             log_error('Non-count values detected in data matrix while data format is \
 set to raw read counts.')
         elif self.alonabase.params['dataformat'] == 'log2':
-            if np.any(data>1000):
+            if np.any(self.data > 1000):
                 log_error('Data do not appear to be log2 transformed.')
         else:
             logging.debug('_validate_counts() finished without a problem')
-            
+
     def _remove_empty(self):
         """ Removes empty cells and genes """
         data_zero = self.data == 0
@@ -51,21 +51,21 @@ set to raw read counts.')
         total_cells = self.data.shape[1]
 
         if np.sum(cells == total_cells) > 0:
-            log_info('%s empty cells will be removed' % (np.sum(cells==total_cells)))
-            self.data = self.data.loc[self.data.index[np.logical_not(cells==total_cells)]]
+            log_info('%s empty cells will be removed' % (np.sum(cells == total_cells)))
+            self.data = self.data.loc[self.data.index[np.logical_not(cells == total_cells)]]
         if np.sum(genes == total_genes) > 0:
-            log_info('%s empty genes will be removed' % (np.sum(genes==total_genes)))
-            self.data = self.data[self.data.columns[np.logical_not(genes==total_genes)]]
-            
+            log_info('%s empty genes will be removed' % (np.sum(genes == total_genes)))
+            self.data = self.data[self.data.columns[np.logical_not(genes == total_genes)]]
+
     def _remove_mito(self):
         """ Remove mitochondrial genes. """
         if self.alonabase.params['nomito']:
             logging.debug('removing mitochondrial genes')
 
-            mt_count = self.data.index.str.contains('^mt-',regex=True, case=False)
-            
+            mt_count = self.data.index.str.contains('^mt-', regex=True, case=False)
+
             if np.sum(mt_count) > 0:
-                log_info('detected and removed %s mitochondrial genes' % np.sum(mt_count))
+                log_info('detected and removed %s mitochondrial gene(s)' % np.sum(mt_count))
                 self.data = self.data.loc[self.data.index[np.logical_not(mt_count)]]
 
     def load_data(self):
@@ -82,7 +82,7 @@ set to raw read counts.')
         # remove duplicate genes (another check)
         if np.any(self.data.index.duplicated(False)):
             log_info('%s duplicated genes still detected, removing them.' % np.sum(
-            self.data.index.duplicated(False)))
+                self.data.index.duplicated(False)))
             self.data = self.data.iloc[np.logical_not(self.data.index.duplicated(False))]
 
         self._validate_counts()
