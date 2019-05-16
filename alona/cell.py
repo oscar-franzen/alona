0d2c51ab43e14c18f6bb05223db7a62ded2ddff6
rand <p.oscar.franzen@gmail.com>
2019-05-16 10:43:13 +0200

    _read_counts_per_cell_filter()

--- alona/cell.py
+++ alona/cell.py
@@ -16,7 +16,10 @@
 
 import logging
 import pandas as pd
-import numpy  as np
+import numpy as np
+
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 
 from .log import (log_info, log_error)
 
@@ -43,9 +46,10 @@ set to raw read counts.')
 
     def _remove_empty(self):
         """ Removes empty cells and genes """
+        # TODO: fix variable names for cells and genes (16-May-2019)
         data_zero = self.data == 0
-        cells = data_zero.sum(axis=1) # 1 = columns
-        genes = data_zero.sum(axis=0) # 0 = rows
+        cells = data_zero.sum(axis=1) # 1 = rows
+        genes = data_zero.sum(axis=0) # 0 = columns
 
         total_genes = self.data.shape[0]
         total_cells = self.data.shape[1]
@@ -67,10 +71,34 @@ set to raw read counts.')
             if np.sum(mt_count) > 0:
                 log_info('detected and removed %s mitochondrial gene(s)' % np.sum(mt_count))
                 self.data = self.data.loc[self.data.index[np.logical_not(mt_count)]]
-                
-    def _quality_filtering():
-        """ Performs basic quality filtering on the data. """
-        pass
+
+    def _read_counts_per_cell_filter(self):
+        """ Generates a bar plot of read counts per cell. """
+        if self.alonabase.params['dataformat'] == 'raw':
+            cell_counts = self.data.sum(axis=0)
+
+            plt.clf()
+
+            no_cells_remove = np.sum(np.array(sorted(cell_counts, reverse=True)) < 1000)
+            colors = ['#E69F00']*(len(cell_counts)-no_cells_remove) + \
+                     ['#999999']*no_cells_remove
+
+            plt.bar(np.arange(len(cell_counts)), sorted(cell_counts, reverse=True),
+                    color=colors)
+            plt.ylabel('raw read counts')
+            plt.xlabel('cells (sorted on highest to lowest)')
+            plt.axhline(linewidth=1, color='r', y=1000)
+            plt.legend(handles=[
+                mpatches.Patch(color='#E69F00', label='Passed'),
+                mpatches.Patch(color='#999999', label='Removed')
+            ])
+            plt.savefig('test.pdf', figsize=(5, 5))
+            plt.close()
+
+            #log_info('Keeping %s out of %s cells' % (
+            #     np.sum(cell_counts>1000), len(cell_counts)))
+
+            #self.data = self.data[self.data.columns[cell_counts>1000]]
 
     def load_data(self):
         """ Load expression matrix. """
@@ -92,5 +120,6 @@ set to raw read counts.')
         self._validate_counts()
         self._remove_empty()
         self._remove_mito()
+        self._read_counts_per_cell_filter()
 
         logging.debug('done loading expression matrix')
