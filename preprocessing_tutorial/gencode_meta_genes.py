97c0193363dbd10d2d5c6d61504663c99ffa251a
rand <p.oscar.franzen@gmail.com>
2019-05-29 10:10:49 +0200

    typos

--- preprocessing_tutorial/gencode_meta_genes.py
+++ preprocessing_tutorial/gencode_meta_genes.py
@@ -14,84 +14,74 @@
 """
 
 import re
-
 from collections import defaultdict
 
 def merge_intervals(intervals):
     """ merge a list of intervals """
     loi = []
-    
+
     for item in intervals:
-        loi.append([ item['annotation_start'], item['annotation_stop'] ])
-    
+        loi.append([ item['anno_start'], item['anno_stop'] ])
+
     loi.sort()
-    
+
     res = []
-    
-    while ( len(loi) > 0 ):
+
+    while (len(loi) > 0):
         if len(loi) == 1:
             res.append(loi[0])
-            
             loi.pop(0)
             continue
-            
+
         if loi[0][1] >= loi[1][0]:
-            tmp = [ loi[0][0], max( loi[0][1], loi[1][1] ) ]
-            
+            tmp = [loi[0][0], max(loi[0][1], loi[1][1])]
             loi[0] = tmp
-            
             loi.pop(1)
-            
             continue
-        
+
         res.append(loi[0])
         loi.pop(0)
-    
+
     return res
 
 def main():
     coordinates = defaultdict(list)
     tx_types = defaultdict(defaultdict)
-    
-    with open('gencode.vM21.annotation.gtf','r') as f:
+
+    with open('gencode.vM21.annotation.gtf', 'r') as f:
         for line in f:
             # ignore comments
             if line[0] == '#':
                 continue
-            
             # ignore exons with retained introns
-            if re.search('retained_intron',line):
+            if re.search('retained_intron', line):
                 continue
-                
+
             foo = line.split('\t')
-            
-            chromosome,
-            annotation_type,
-            annotation_start,
-            annotation_stop,
-            annotation_strand = foo[0],foo[2],foo[3],foo[4],foo[6]
-            
-            if (int(annotation_start)-int(annotation_stop)) == 0:
+
+            chrm,anno_type,anno_start,anno_stop,anno_strand = foo[0],foo[2],foo[3],foo[4],foo[6]
+
+            if (int(anno_start) - int(anno_stop)) == 0:
                 continue
-                
-            if annotation_type == 'exon':
+
+            if anno_type == 'exon':
                 # collect identifiers
-                gene_symbol = re.search('gene_name "(\S+)"',line).group(1)
-                gene_ens = re.search('gene_id "(\S+)\..+"',line).group(1)
+                gene_symbol = re.search('gene_name "(\S+)"', line).group(1)
+                gene_ens = re.search('gene_id "(\S+)\..+"', line).group(1)
+
+                gene_id = '%s_%s' % (gene_symbol, gene_ens)
                 
-                gene_id = '%s_%s' % (gene_symbol,gene_ens)
-                
-                tx_type = re.search('transcript_type "(.*?)"',line).group(1)
+                tx_type = re.search('transcript_type "(.*?)"', line).group(1)
                 tx_types[gene_id][tx_type] = 1
                 
-                coordinates[gene_id].append({ 'annotation_start' : annotation_start,
-                                              'annotation_stop' : annotation_stop,
-                                              'chr' : chromosome,
-                                              'annotation_strand' : annotation_strand })
+                coordinates[gene_id].append({'anno_start' : anno_start,
+                                             'anno_stop' : anno_stop,
+                                             'chr' : chrm,
+                                             'anno_strand' : anno_strand})
     
-    for gene_id in coordinates:        
+    for gene_id in coordinates:
         merged = merge_intervals(coordinates[gene_id])
-        
+
         for item in merged:
             #print("$chr\tGENCODE.v27.custom\texon\t$start\t$stop\t1000\t
             #$strand\t.\tgene_id \"$gene\"; gene_name \"$gene\"; transcript_id
@@ -104,7 +94,7 @@ gene_name "%s"; transcript_id "%s"; transcript_types "%s"' %
                 (coordinates[gene_id][0]['chr'],
                 item[0],
                 item[1],
-                coordinates[gene_id][0]['annotation_strand'],
+                coordinates[gene_id][0]['anno_strand'],
                 gene_id,
                 gene_id,
                 gene_id,
