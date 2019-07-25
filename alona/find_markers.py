4d882d6ab7cb0da8394296635c6b11554f641eae
rand <p.oscar.franzen@gmail.com>
2019-07-25 11:22:46 +0200

    find_markers.py

---
+++ alona/find_markers.py
@@ -0,0 +1,14 @@
+"""
+ alona
+
+ Description:
+ An analysis pipeline for scRNA-seq data.
+
+ How to use:
+ https://github.com/oscar-franzen/alona/
+
+ Contact:
+ Oscar Franzen <p.oscar.franzen@gmail.com>
+"""
+
+import sys
